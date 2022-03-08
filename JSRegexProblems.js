0fc7e36600c3053173a4f2c80a5f0251e0cfada4
NAVEEN MABBU <95521452+naveenmabbu@users.noreply.github.com>
2022-03-09 00:15:43 +0530

    UC4 of JSRegexProblem program

---
+++ JSRegexProblems.js
@@ -0,0 +1,100 @@
+/* Patterns or Regex:-
+        Patterns or Regular expressions are special characters which help search data, matching
+        complex patterns. Regular expressions are shortened as 'regexp' or 'regex’.
+*/
+
+/* UC 1: The Postal Index Number (PIN) or PIN Code is a 6 digit code of Post Office numbering used
+by India Post. Create a regex pattern to validate PIN Code UC 1 PIN code 400088
+*/
+{
+    let validateWithoutSpace = () => {
+        console.log("\n***** UC1 *****");
+        try {
+            let pincode = "400088"//prompt("Enter pincode:");
+            //regex pattern for pincode not allowing spaces
+            let pincodePattern = new RegExp('^[1-9]{1}[0-9]{5}$');
+            //test pincode with pattern
+            if (pincodePattern.test(pincode))
+                console.log('Pincode is valid');
+            else
+                throw 'Error! Pincode is not valid';
+        }
+        catch (e) {
+            //catch the error and print it
+            console.error(e);
+        }
+    }
+    validateWithoutSpace();
+}
+
+/* UC 2: Restrict the PIN code from taking alphabets or special characters at the beginning.
+Check for A400088 – this should fail
+*/
+{
+    let validatePINCode = () => {
+        console.log("\n***** UC2 *****");
+        try {
+            let pincode = "A400088"//prompt("Enter pincode:");
+            //regex pattern for pincode not allowing spaces
+            let pincodePattern = new RegExp('^[1-9]{1}[0-9]{5,}$');
+            //test pincode with pattern
+            if (pincodePattern.test(pincode))
+                console.log('Pincode is valid');
+            else
+                throw 'Error! Pincode is not valid';
+        }
+        catch (e) {
+            //catch the error and print it
+            console.error(e);
+        }
+    }
+    validatePINCode();
+}
+
+/* UC 3: Restrict the PIN code from taking alphabets or special characters at the End.
+Check for 400088B – this should fail
+*/
+{
+    let validatePINCode = () => {
+        console.log("\n***** UC3 *****");
+        try {
+            let pincode = "400088B"//prompt("Enter pincode:");
+            //regex pattern for pincode not allowing spaces
+            let pincodePattern = new RegExp('^[1-9]{1}[0-9]{5,}$');
+            //test pincode with pattern
+            if (pincodePattern.test(pincode))
+                console.log('Pincode is valid');
+            else
+                throw 'Error! Pincode is not valid';
+        }
+        catch (e) {
+            //catch the error and print it
+            console.error(e);
+        }
+    }
+    validatePINCode();
+}
+
+/* UC 4: Make sure 400 088 is also valid along with 400088
+*/
+{
+    let validatePINCode = () => {
+        console.log("\n***** UC4 *****");
+        try {
+            const prompt=require("prompt-sync")();  // For Taking input from user
+            let pincode = prompt("Enter pincode: ");// string ex- 400 088
+            //regex pattern for pincode not allowing spaces
+            let pincodePattern = new RegExp('^[1-9]{1}[0-9]{2}\\s?[0-9]{3}$');
+            //test pincode with pattern
+            if (pincodePattern.test(pincode))
+                console.log('Pincode is valid');
+            else
+                throw 'Error! Pincode is not valid';
+        }
+        catch (e) {
+            //catch the error and print it
+            console.error(e);
+        }
+    }
+    validatePINCode();
+}
